8677dd5420c2edd80ee3cfeeb7e84c829f5a3b78
forhadhossen1 <mr.forhad1030@gamil.com>
2024-02-21 22:30:08 +0600

    create project with tailwind react

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import NewsFormate from "./NewsFormate"
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <div>
+          <h2 className="text-center font-bold text-3xl border-b-4 border-b-gray-500 w-1/3 mx-auto py-5">News Formate</h2>
+
+
+          <NewsFormate></NewsFormate>
+        </div>
+
+        <div>
+          <h2 className="text-center font-bold text-3xl border-b-4 border-b-gray-500 w-1/3 mx-auto py-5">NavBar</h2>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
